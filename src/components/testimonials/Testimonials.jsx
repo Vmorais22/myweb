5e42550335a119cd5025e945300a22ed0552b924
Victor <victormoraisllahi@gmail.com>
2021-10-03 13:47:45 +0200

    testimonials modified

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,69 +1,63 @@
 import "./testimonials.scss";
-import rightArrow from "../../assets/images/right-arrow.png"
-import twitter from "../../assets/images/twitter.png"
-import youtube from "../../assets/images/youtube.png"
-import linkedin from "../../assets/images/linkedin.png"
 import {useTranslation} from "react-i18next";
 import construction from "../../assets/images/pngjoy.com_coming-soon-sign-site-under-construction-png-transparent_9515370.png"
+import img1 from "../../assets/images/CuerdosYLocosPortada.PNG";
+import img2 from "../../assets/images/verumIstaPortada.PNG";
+import img3 from "../../assets/images/carpeDiemPortada.PNG";
 
 export default function Testimonials() {
-    const ready = false;
+    const ready = true;
     const [t] = useTranslation("global");
     const data = [
         {
             id: 1,
-            name: "Tom Durden",
-            title: "Senior Developer",
+            name: "Verum Ista",
+            title: "2018/2019",
             img:
-                "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-            icon: twitter,
+                img2,
             desc:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+                "Quién le iba a decir a Adrián que el oscuro secreto que ha estado guardando estos últimos 17 años correría el riesgo de salir a la luz cuando un terrorista amenaza con atentar contra España si no le entregan el REG-7. Descubre la secuela de Carpe Diem cargada de acción, intriga y suspense coprotagonizada esta vez por dos Mendozas.",
         },
         {
             id: 2,
-            name: "Alex Kalinski",
-            title: "Co-Founder of DELKA",
+            name: "Cuerdos y Locos",
+            title: "2020",
             img:
-                "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-            icon: youtube,
+            img1,
             desc:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+                "En medio de la inmensidad del mar hay una remota isla... En una remota isla hay una profunda selva... Y en una profunda selva hay la cárcel psiquiatrica más grande y segura del planeta. ¿Cómo podemos decir que alguien está loco si no sabemos describir que es la cordura? Delaré Cooper, el último fichaje de la institución, tendrá que enfrentarse a ese dilema y a muchos más.",
             featured: true,
         },
         {
             id: 3,
-            name: "Martin Harold",
-            title: "CEO of ALBI",
+            name: "Carpe Diem",
+            title: "2017/2018",
             img:
-                "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-            icon: linkedin,
+            img3,
             desc:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+                "Adrián Mendoza tiene un don muy especial. Puede controlar a su voluntad el curso de sus sueños y vivir lo que se le antoje mientras duerme. Qué poco imagina él que esa vida tan dispar de la rutina aburrida que crea por las noches muy pronto va a dejar de ser ficción cuando el Equipo Táctico de Inteligencia le reclute para una misión especial.",
         },
     ];
     return (
 
         <div className="testimonials" id="testimonials">
-            {(ready) ? (<h1>{t("novel.title")}</h1>,
-                <div className="container">
+            <h1>{t("novel.title")}</h1>
+            {(ready) ? (
+                <div className="container" >
                     {data.map((d) => (
-                        <div className={d.featured ? "card featured" : "card"}>
-                            <div className="top">
-                                <img src={rightArrow} className="left" alt=""/>
-                                <img
-                                    className="user"
-                                    src={d.img}
-                                    alt=""
-                                />
-                                <img className="right" src={d.icon} alt=""/>
-                            </div>
+                        <div className={d.featured ? "card featured" : "card"} style={{
+                            backgroundImage: `url(${d.img})`
+                        }}>
                             <div className="center">
-                                {d.desc}
+                                <p>{d.desc}</p>
                             </div>
                             <div className="bottom">
                                 <h3>{d.name}</h3>
                                 <h4>{d.title}</h4>
+                            </div>
+                            <div className="buttonContainer">
+                                <a href="/"><span>{t("pre")}</span></a>
+                                <a href="/"><span>{t("buy")}</span></a>
                             </div>
                         </div>
                     ))}
